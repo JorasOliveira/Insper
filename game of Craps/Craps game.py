3787a784fff235287294b79c8c31237ac1be84de
Joras Oliveira <43121361+JorasOliveira@users.noreply.github.com>
2020-04-03 15:53:44 -0300

    Add files via upload

--- game of Craps/Craps game.py
+++ game of Craps/Craps game.py
@@ -12,37 +12,55 @@ def comeOut(money):
     dado2 = random.randrange(1, 6)
     soma = dado1 + dado2
 
+
 def passLine(aposta, soma):
     if soma is 7:
-        print("ganhou a aposta Pass Line!\n")
+        print("ganhou a aposta Pass Line!")
         return aposta * 2
     elif (soma is 2) or (soma is 3) or (soma is 12):
-        print("perdeu a aposta Pass Line\n")
+        print("perdeu a aposta Pass Line")
         return -aposta
     else:
         fase = 1
-        print("agora vamos pra fase 2: Point\n")
+        print("agora vamos pra fase 2: Point")
         return None
 
 
 def field(aposta, soma):
     if soma is 2:
-        print("Voce Ganhou o dobro!!\n")
-        return aposta + aposta*2
+        print("Voce Ganhou o dobro!!")
+        return aposta + aposta * 2
     if soma is 12:
-        print("Voce Ganhou o triplo!!!\n")
-        return aposta + aposta*3
-    if soma is 3 or 4 or 9 or 11:
-        print("ganhou a aposta Field!\n")
+        print("Voce Ganhou o triplo!!!")
+        return aposta + aposta * 3
+    if soma is 3 or soma is 4 or soma is 9 or soma is 11:
+        print("ganhou a aposta Field!")
         return aposta * 2
-    if soma is 5 or 6 or 7 or 8:
-        print("perdeu a aposta Field\n")
+    if soma is 5 or soma is 6 or soma is 7 or soma is 8:
+        print("perdeu a aposta Field")
         return -aposta
-    # elif (dado1 or dado2) is 3 or 4 or 9 or 11:
-    #     print("ganhou a aposta Field!\n")
-    #     return aposta*2
+    return None
+
+
+def anyCraps(aposta, soma):
+    if soma is 2 or soma is 3 or soma is 12:
+        print("Parabens, voce ganhou a aposta Any Craps!")
+        return  aposta * 7
+    else:
+        print("voce perdeu a aposta Any Craps")
+        return -aposta
+
+def twelve(aposta, soma):
+    if soma is 12:
+        print("parabens voce ganhou a aposta twelve!")
+        return aposta * 30
+    else:
+        print("voce perdeu a aposta twelve")
+        return - aposta
 
 
 comeOut(fichas)
 print(passLine(5, 5))
-print(field(5, 3))
+print(field(5, 8))
+print(anyCraps(5, 8))
+print(twelve(5, 12))
